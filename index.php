d6410b149cf7e1feb278cec62c6105f50ee21ca2
A-I-N-A-Z <69522453+A-I-N-A-Z@users.noreply.github.com>
2022-01-30 16:20:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
-    $arr = [];
-    setcookie("lol", counter());
-    function counter() {
-        if(isset($_COOKIE["lol"])){
-            $_COOKIE["lol"]++;
-        } else{
-            $_COOKIE["lol"] = 1;
-        };
-        return $_COOKIE["lol"];
-    };
-    $arr[$_SERVER["REMOTE_ADDR"]] = $_COOKIE["lol"];
-    foreach($arr as $index => $val){
-        $str = $index . " : " . $val . "; 
-";
-    }
-    file_put_contents("content.txt", file_get_contents("content.txt") . $str);
-    header("location: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    echo "<h1>" . $_SERVER["REMOTE_ADDR"] . "</h1>";
 ?>
